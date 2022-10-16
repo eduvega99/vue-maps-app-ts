0152c9d02f3463eefbacf301097cd49cd0fce7ee
Eduardo Vega Rodríguez <eduardovr1999@gmail.com>
2022-10-16 22:08:40 +0200

    Show polyline

--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -28,6 +28,56 @@ const mutation: MutationTree<MapState> = {
                 .addTo(state.map!);
             state.markers.push(marker)
         }
+    },
+    setRoutePolilyne(state, coords: number[][]) {
+        const start = coords[0];
+        const end = coords[coords.length - 1];
+
+        const bounds = new Mapboxgl.LngLatBounds(
+            [start[0], start[1]],
+            [end[0], end[1]],
+        );
+        for (const coord of coords) {
+            const newCoord: [number, number] = [coord[0], coord[1]];
+            bounds.extend(newCoord);
+        }
+        state.map?.fitBounds(bounds, {
+            padding: 300
+        });
+        const sourceData: Mapboxgl.AnySourceData = {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: coords
+                        }
+                    }
+                ]
+            }
+        };
+        if (state.map?.getLayer('RouteString')) {
+            state.map?.removeLayer('RouteString');
+            state.map?.removeSource('RouteString');
+        }
+        state.map?.addSource('RouteString', sourceData);
+        state.map?.addLayer({
+            id: 'RouteString',
+            type: 'line',
+            source: 'RouteString',
+            layout: {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            paint: {
+                'line-color': 'black',
+                'line-width': 3
+            }
+        })
     }
 }
 
